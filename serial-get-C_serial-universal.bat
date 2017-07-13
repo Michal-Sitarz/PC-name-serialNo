756a3b25c8298046c7af1737e4c025a4885d999d
Michal Sitarz <me@michalsitarz.com>
2017-07-13 10:53:31 +0100

    save-file-on-C-drive version working

---
+++ serial-get-C_serial-universal.bat
@@ -0,0 +1,65 @@
+@ECHO OFF
+REM /* Author: Michal Sitarz • June 2017 • Tokheim IT Department
+REM  *
+REM  * Description:
+REM  * Small script to obtain a serial number 
+REM  * from Windows based OS computers.
+REM  */
+
+REM /// get SERIAL NUMBER functionality
+REM ===================================
+
+REM // obtain a value of a serial number 
+REM // and set it to the variable: %serial%
+FOR /f "tokens=2 delims==" %%f IN ('wmic bios get serialnumber /value ^| FIND "="') DO SET "serial=%%f"
+
+
+REM /// add MANUFACTURER's PREFIX functionality
+REM ===========================================
+
+REM -----------------------------------------------------------
+REM --- add prefix depending on the computer manufacturer:  ---
+REM --- H for HP, D for Dell                                ---
+REM -----------------------------------------------------------
+
+REM // check the manufacturer
+FOR /f "tokens=2 delims==" %%m IN ('wmic bios get manufacturer /value ^| FIND "="') DO SET "manufacturer=%%m"
+
+REM // set the right prefix for corresponding manufacturer,
+REM // if accidentally used other than approved manufacturers, then use "Other" as a prefix
+
+IF %manufacturer% == HP (
+	SET prefix=H
+	) ELSE IF %manufacturer% == Dell (
+			SET prefix=D
+			) ELSE (
+				SET prefix=Other
+				) 
+
+REM // merge prefix with generated serial number
+SET serial=%prefix%%serial%
+
+
+REM /// CLIPBOARD functionality
+REM ===========================
+REM // output the variable %serial% to the clipboard
+@ECHO %serial% | clip
+
+
+REM /// FILE SAVE functionality
+REM ===========================
+REM // set the path to the desired directory
+SET currPath=C:\serial
+
+REM // check if the folder exists, if not: create it
+IF NOT EXIST %currPath% MKDIR %currPath%
+
+REM // set file name
+SET path="%currPath%\serial.txt"
+
+REM // check if the file exist: delete it, if does
+IF EXIST %path% DEL %path%
+
+REM // output the variable %serial% into a file
+@ECHO %serial% >> %path%
+
